10ee0f93390001a32eaef1ab9dc4fd0857454ec3
javier-escribano <javier.escribano@udc.es>
2020-12-10 23:57:04 +0100

    Concurrencia implementada

--- ipm-p1.py
+++ ipm-p1.py
@@ -4,14 +4,17 @@ import gi
 import json
 import random
 import webbrowser
+import threading
+import time
 
 gi.require_version("Gtk", "3.0")
-from gi.repository import Gtk
-from urllib import request
+from gi.repository import Gtk, GLib
+from urllib import request, error
 
 url_songs = "http://localhost:5000/songs"
 url_intervals = "http://localhost:5000/intervals"
 notes = ["do", "do♯", "re", "re♯", "mi", "fa", "fa♯", "sol", "sol♯", "la", "la♯", "si"]
+LOAD = 0.3
 
 
 def get_interval_distance(interval):
@@ -32,29 +35,39 @@ def get_interval_distance(interval):
     return switcher.get(interval, "Intervalo Inválido")
 
 
-def get_songs(interval, asc_des):
-    response = request.urlopen(url_songs + "/" + interval + "/" + asc_des)
-    data = response.read()
-    jsondata = json.loads(data)
-    listdata = jsondata["data"]
-    response_list = []
+def get_songs(window):
+    try:
+        response = request.urlopen(url_songs + "/" + window.interval + "/" + window.on_off)
+        data = response.read()
+        jsondata = json.loads(data)
+        listdata = jsondata["data"]
+        response_list = []
 
-    for i in range(len(listdata)):
-        response_list.append(listdata[i])
+        for i in range(len(listdata)):
+            response_list.append(listdata[i])
 
-    return response_list
+        time.sleep(LOAD)
+        GLib.idle_add(window.songs_response, 1, response_list)
+    except error.URLError:
+        time.sleep(LOAD)
+        GLib.idle_add(window.songs_response, 0, None)
 
 
-def get_intervals():
-    response = request.urlopen(url_intervals)
-    data = response.read()
-    jsondata = json.loads(data)
-    response_list = []
+def get_intervals(window):
+    try:
+        response = request.urlopen(url_intervals)
+        data = response.read()
+        jsondata = json.loads(data)
+        response_list = []
 
-    for interval in jsondata["data"]:
-        response_list.append(interval)
+        for interval in jsondata["data"]:
+            response_list.append(interval)
 
-    return response_list
+        time.sleep(LOAD)
+        GLib.idle_add(window.interval_response, 1, response_list)
+    except error.URLError:
+        time.sleep(LOAD)
+        GLib.idle_add(window.interval_response, 0, None)
 
 
 def create_vbox():
@@ -73,81 +86,105 @@ class ResponseWindow(Gtk.Window):
     def __init__(self, interval, on_off):
         # Vars
         self.interval = interval
+        self.on_off = on_off
 
         # Window
         Gtk.Window.__init__(self, title="Respuesta: " + interval + " " + on_off)
         self.set_size_request(500, 300)
 
+        # Thread
+        self.songs_petition()
+
         # Container
-        container_box = create_vbox()
-        container_box.set_valign(Gtk.Align.START)
-        container_box.set_border_width(30)
+        self.container_box = create_vbox()
+        self.container_box.set_valign(Gtk.Align.START)
+        self.container_box.set_border_width(30)
 
         # Header
-        interval_label = Gtk.Label()
-        interval_label.set_markup("<b><span size='x-large'>Intervalo: </span></b>" + "<span size='x-large'>" +
-                                  interval + " " + on_off + "</span>")
-        interval_label.set_halign(Gtk.Align.START)
-        example_label = Gtk.Label()
-        example = self.create_example(on_off)
-        example_label.set_markup(
-            "<b><span size='x-large'>Ejemplo: </span></b>" + "<span size='x-large'>" + example + "</span>")
-        example_label.set_halign(Gtk.Align.START)
-        example_label.set_margin_top(10)
+        self.interval_label = Gtk.Label()
+        self.interval_label.set_markup("<b><span size='x-large'>Intervalo: </span></b>" + "<span size='x-large'>" +
+                                       interval + " " + on_off + "</span>")
+        self.interval_label.set_halign(Gtk.Align.START)
+        self.example_label = Gtk.Label()
+        self.example = self.create_example(on_off)
+        self.example_label.set_markup(
+            "<b><span size='x-large'>Ejemplo: </span></b>" + "<span size='x-large'>" + self.example + "</span>")
+        self.example_label.set_halign(Gtk.Align.START)
+        self.example_label.set_margin_top(10)
 
-        container_box.pack_start(interval_label, True, True, 0)
-        container_box.pack_start(example_label, True, True, 0)
+        self.container_box.pack_start(self.interval_label, True, True, 0)
+        self.container_box.pack_start(self.example_label, True, True, 0)
 
         # Treeview
         self.liststore = Gtk.ListStore(str, str, str)
         self.fav_filter = self.liststore.filter_new()
         self.fav_filter.set_visible_func(self.fav_filter_func)
-        treeview = Gtk.TreeView(model=self.fav_filter)
-        treeview.set_margin_top(30)
+        self.treeview = Gtk.TreeView(model=self.fav_filter)
+        self.treeview.set_margin_top(30)
         self.current_fav_filter = "NO"
 
-        renderer_title = Gtk.CellRendererText()
-        column_title = Gtk.TreeViewColumn("Título", renderer_title, text=0)
-        column_title.set_resizable(True)
-        treeview.append_column(column_title)
+        self.renderer_title = Gtk.CellRendererText()
+        self.column_title = Gtk.TreeViewColumn("Título", self.renderer_title, text=0)
+        self.column_title.set_resizable(True)
+        self.treeview.append_column(self.column_title)
 
-        renderer_url = Gtk.CellRendererText()
-        column_url = Gtk.TreeViewColumn("Url", renderer_url, text=1)
-        column_url.set_resizable(True)
-        treeview.append_column(column_url)
+        self.renderer_url = Gtk.CellRendererText()
+        self.column_url = Gtk.TreeViewColumn("Url", self.renderer_url, text=1)
+        self.column_url.set_resizable(True)
+        self.treeview.append_column(self.column_url)
 
-        renderer_fav = Gtk.CellRendererText()
-        column_fav = Gtk.TreeViewColumn("Favoritos", renderer_fav, text=2)
-        column_fav.set_resizable(True)
-        treeview.append_column(column_fav)
+        self.renderer_fav = Gtk.CellRendererText()
+        self.column_fav = Gtk.TreeViewColumn("Favoritos", self.renderer_fav, text=2)
+        self.column_fav.set_resizable(True)
+        self.treeview.append_column(self.column_fav)
 
-        treeview.connect("row-activated", self.open_url)
+        self.treeview.connect("row-activated", self.open_url)
 
-        container_box.pack_start(treeview, False, False, 0)
+        self.container_box.pack_start(self.treeview, False, False, 0)
 
         # Fav Switcher Section
-        fav_box = Gtk.Box(spacing=15)
-        fav_box.set_orientation(Gtk.Orientation.HORIZONTAL)
-        fav_box.set_halign(Gtk.Align.CENTER)
-        fav_box.set_margin_top(30)
-        fav_label = Gtk.Label()
-        fav_label.set_markup("<b><span size='x-large'>Sólo Favoritos</span></b>")
-        fav_switch = create_switch()
-        fav_switch.connect("notify::active", self.on_switch_used, fav_switch)
-        fav_switch.set_halign(Gtk.Align.CENTER)
+        self.fav_box = Gtk.Box(spacing=15)
+        self.fav_box.set_orientation(Gtk.Orientation.HORIZONTAL)
+        self.fav_box.set_halign(Gtk.Align.CENTER)
+        self.fav_box.set_margin_top(30)
+        self.fav_label = Gtk.Label()
+        self.fav_label.set_markup("<b><span size='x-large'>Sólo Favoritos</span></b>")
+        self.fav_switch = create_switch()
+        self.fav_switch.connect("notify::active", self.on_switch_used, self.fav_switch)
+        self.fav_switch.set_halign(Gtk.Align.CENTER)
 
-        fav_box.pack_start(fav_label, True, True, 0)
-        fav_box.pack_start(fav_switch, True, True, 0)
+        self.fav_box.pack_start(self.fav_label, True, True, 0)
+        self.fav_box.pack_start(self.fav_switch, True, True, 0)
 
-        container_box.pack_start(fav_box, False, False, 0)
+        self.container_box.pack_start(self.fav_box, False, False, 0)
+
+        # Default & Error view
+        self.spin = Gtk.Spinner()
+        self.spin.set_halign(Gtk.Align.CENTER)
+        self.spin.set_size_request(35, 35)
+        self.spin.start()
+
+        self.error_h1 = Gtk.Label()
+        self.error_h1.set_markup("<b><span size='xx-large'>Error</span></b>")
+        self.error_p = Gtk.Label()
+        self.error_p.set_markup("No se ha podido conectar con el servidor")
+        self.error_button = Gtk.Button.new_with_label(label="Reintentar")
+        self.error_button.set_margin_top(20)
+        self.error_button.set_size_request(30, 20)
+        self.error_button.connect("clicked", self.on_retry_clicked)
+
+        self.container_box.pack_start(self.error_h1, True, True, 0)
+        self.container_box.pack_start(self.error_p, True, True, 0)
+        self.container_box.pack_start(self.error_button, False, False, 0)
+        self.container_box.pack_start(self.spin, True, True, 0)
 
         # Add
-        self.fill_treeview(self.liststore, interval, on_off)
-        self.add(container_box)
+        self.add(self.container_box)
+
+        self.default_view()
 
     @staticmethod
-    def fill_treeview(liststore, interval, on_off):
-        interval_list = get_songs(interval, on_off)
+    def fill_treeview(liststore, interval, on_off, interval_list):
 
         for i in range(len(interval_list)):
             liststore.append([interval_list[i][0], interval_list[i][1], interval_list[i][2]])
@@ -157,6 +194,13 @@ class ResponseWindow(Gtk.Window):
             return True
         else:
             return model[iteri][2] == "YES"
+
+    def on_retry_clicked(self, widget):
+        self.error_button.hide()
+        self.error_h1.hide()
+        self.error_p.hide()
+        self.spin.start()
+        self.songs_petition()
 
     def on_switch_used(self, widget, gparam, switch):
         if switch.get_active():
@@ -175,6 +219,30 @@ class ResponseWindow(Gtk.Window):
             snd_note = (fst_note - distance) % 12
             return notes[fst_note] + " - " + notes[snd_note]
 
+    def default_view(self):
+        self.container_box.set_valign(Gtk.Align.CENTER)
+        self.container_box.show()
+        self.spin.show()
+
+    def songs_petition(self):
+        thread = threading.Thread(target=get_songs, args=(self,))
+        thread.daemon = True
+        thread.start()
+
+    def songs_response(self, control, songs_list):
+        self.spin.stop()
+        if control == 0:
+            self.error_h1.show()
+            self.error_p.show()
+            self.error_button.show()
+        else:
+            self.container_box.set_valign(Gtk.Align.START)
+            self.interval_label.show()
+            self.example_label.show()
+            self.fill_treeview(self.liststore, self.interval, self.on_off, songs_list)
+            self.treeview.show_all()
+            self.fav_box.show_all()
+
     def open_url(self, view, row, column):
         if self.liststore[row][1] != "":
             webbrowser.open(self.liststore[row][1])
@@ -186,38 +254,80 @@ class MainWindow(Gtk.Window):
         Gtk.Window.__init__(self, title="Interfaz Musical")
         self.set_size_request(600, 400)
 
+        # Create thread for interval petition
+        self.interval_petition()
+
         # Main Container
-        container_box = create_vbox()
-        container_box.set_valign(Gtk.Align.START)
-        container_box.set_border_width(30)
+        self.container_box = create_vbox()
+        self.container_box.set_valign(Gtk.Align.START)
+        self.container_box.set_border_width(30)
 
         # Switcher Section
-        asc_label = Gtk.Label()
-        asc_label.set_markup("<b><span size='x-large'>Intervalo Ascendente</span></b>")
-        asc_switch = create_switch()
-        asc_switch.set_halign(Gtk.Align.CENTER)
-        asc_switch.set_margin_top(20)
+        self.asc_label = Gtk.Label()
+        self.asc_label.set_markup("<b><span size='x-large'>Intervalo Ascendente</span></b>")
+        self.asc_switch = create_switch()
+        self.asc_switch.set_halign(Gtk.Align.CENTER)
+        self.asc_switch.set_margin_top(20)
 
-        container_box.pack_start(asc_label, True, True, 0)
-        container_box.pack_start(asc_switch, True, True, 0)
+        self.container_box.pack_start(self.asc_label, True, True, 0)
+        self.container_box.pack_start(self.asc_switch, True, True, 0)
 
         # Button Section
-        interval_label = Gtk.Label()
-        interval_label.set_margin_top(25)
-        interval_label.set_markup("<b><span size='x-large'>Intervalos</span></b>")
-        interval_label.set_halign(Gtk.Align.START)
-        flowbox = Gtk.FlowBox()
-        flowbox.set_row_spacing(15)
-        flowbox.set_column_spacing(15)
-        flowbox.set_margin_top(20)
-        flowbox.set_max_children_per_line(4)
-        self.create_buttons(flowbox, asc_switch)
+        self.interval_label = Gtk.Label()
+        self.interval_label.set_margin_top(25)
+        self.interval_label.set_markup("<b><span size='x-large'>Intervalos</span></b>")
+        self.interval_label.set_halign(Gtk.Align.START)
+        self.flowbox = Gtk.FlowBox()
+        self.flowbox.set_row_spacing(15)
+        self.flowbox.set_column_spacing(15)
+        self.flowbox.set_margin_top(20)
+        self.flowbox.set_max_children_per_line(4)
 
-        container_box.pack_start(interval_label, True, True, 0)
-        container_box.pack_start(flowbox, False, False, 0)
+        self.container_box.pack_start(self.interval_label, True, True, 0)
+        self.container_box.pack_start(self.flowbox, False, False, 0)
+
+        # Default & Error view
+        self.spin = Gtk.Spinner()
+        self.spin.set_halign(Gtk.Align.CENTER)
+        self.spin.set_size_request(35, 35)
+        self.spin.start()
+
+        self.error_h1 = Gtk.Label()
+        self.error_h1.set_markup("<b><span size='xx-large'>Error</span></b>")
+        self.error_p = Gtk.Label()
+        self.error_p.set_markup("No se ha podido conectar con el servidor")
+        self.error_button = Gtk.Button.new_with_label(label="Reintentar")
+        self.error_button.set_margin_top(20)
+        self.error_button.set_size_request(30, 20)
+        self.error_button.connect("clicked", self.on_retry_clicked)
+
+        self.container_box.pack_start(self.error_h1, True, True, 0)
+        self.container_box.pack_start(self.error_p, True, True, 0)
+        self.container_box.pack_start(self.error_button, False, False, 0)
+        self.container_box.pack_start(self.spin, True, True, 0)
 
         # Add
-        self.add(container_box)
+        self.add(self.container_box)
+
+        # Load default view
+        self.default_view()
+
+    def default_view(self):
+        self.container_box.set_valign(Gtk.Align.CENTER)
+        self.container_box.show()
+        self.spin.show()
+
+    def on_retry_clicked(self, widget):
+        self.error_button.hide()
+        self.error_h1.hide()
+        self.error_p.hide()
+        self.spin.start()
+        self.interval_petition()
+
+    def interval_petition(self):
+        thread = threading.Thread(target=get_intervals, args=(self,))
+        thread.daemon = True
+        thread.start()
 
     @staticmethod
     def switch_status(switch):
@@ -226,22 +336,44 @@ class MainWindow(Gtk.Window):
         else:
             return "des"
 
-    def on_button_clicked(self, widget, interval, switch):
-        on_off = self.switch_status(switch)
+    @staticmethod
+    def create_response(interval, on_off):
         response = ResponseWindow(interval, on_off)
-        response.show_all()
+        response.show()
 
-    def create_buttons(self, flowbox, switch):
-        buttons = get_intervals()
+    def call_response(self, interval, switch):
+        on_off = self.switch_status(switch)
+        GLib.idle_add(self.create_response, interval, on_off)
 
-        for button in buttons:
+    def on_button_clicked(self, widget, interval, switch):
+        thread = threading.Thread(target=self.call_response, args=(interval, switch))
+        thread.daemon = True
+        thread.start()
+
+    def create_buttons(self, flowbox, switch, interval_list):
+
+        for button in interval_list:
             added_item = Gtk.Button.new_with_label(button)
             added_item.set_size_request(30, 70)
             added_item.connect("clicked", self.on_button_clicked, button, switch)
             flowbox.add(added_item)
 
+    def interval_response(self, control, interval_list):
+        self.spin.stop()
+        if control == 0:
+            self.error_h1.show()
+            self.error_p.show()
+            self.error_button.show()
+        else:
+            self.container_box.set_valign(Gtk.Align.START)
+            self.asc_label.show()
+            self.asc_switch.show()
+            self.interval_label.show()
+            self.create_buttons(self.flowbox, self.asc_switch, interval_list)
+            self.flowbox.show_all()
+
 
 win = MainWindow()
 win.connect("destroy", Gtk.main_quit)
-win.show_all()
+win.show()
 Gtk.main()
